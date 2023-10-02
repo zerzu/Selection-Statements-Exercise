054b313392b6bb0e5e0bf4310433eba77187611f
Eric Garner <ericgarner81@gmail.com>
2023-10-02 13:21:50 -0500

    exerxise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+
+            }            
+
+            else if (userInput > favNumber) {
+                
+                Console.WriteLine("Too high.");
+
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
         }
     }
 }
